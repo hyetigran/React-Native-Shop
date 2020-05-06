63cac389fff1e169e8a0a2395479f79149c4ca74
hyetigran <tigran.asriyan@gmail.com>
2020-05-05 22:55:29 -0400

    add fetch orders from db

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { FlatList, Text, Platform } from 'react-native';
-import { useSelector } from 'react-redux';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  Text,
+  Platform,
+  ActivityIndicator,
+  StyleSheet,
+  View,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import * as ordersActions from "../../store/actions/orders";
 
-import HeaderButton from '../../components/UI/HeaderButton';
-import OrderItem from '../../components/shop/OrderItem';
+import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
+import Colors from "../../constants/Colors";
 
-const OrdersScreen = props => {
-  const orders = useSelector(state => state.orders.orders);
+const OrdersScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const orders = useSelector((state) => state.orders.orders);
+  const dispatch = useDispatch();
 
+  const loadOrders = async () => {
+    await dispatch(ordersActions.setOrders);
+  };
+  useEffect(() => {
+    setIsLoading(true);
+    loadOrders();
+    setIsLoading(false);
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="Large" color={Colors.primary} />
+      </View>
+    );
+  }
   return (
     <FlatList
       data={orders}
-      keyExtractor={item => item.id}
-      renderItem={itemData => (
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
         <OrderItem
           amount={itemData.item.totalAmount}
           date={itemData.item.readableDate}
@@ -24,21 +51,23 @@ const OrdersScreen = props => {
   );
 };
 
-OrdersScreen.navigationOptions = navData => {
+OrdersScreen.navigationOptions = (navData) => {
   return {
-    headerTitle: 'Your Orders',
+    headerTitle: "Your Orders",
     headerLeft: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Menu"
-          iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
           onPress={() => {
             navData.navigation.toggleDrawer();
           }}
         />
       </HeaderButtons>
-    )
+    ),
   };
 };
-
+const styles = StyleSheet.create({
+  centered: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
 export default OrdersScreen;
--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,5 +1,38 @@
-export const ADD_ORDER = "ADD_ORDER";
+import Order from "../../models/order";
 
+export const ADD_ORDER = "ADD_ORDER";
+export const SET_ORDERS = "SET_ORDERS";
+
+export const setOrders = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "https://rn-guide-7456a.firebaseio.com/orders.json"
+      );
+      if (!respons.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const resData = await response.json();
+      const loadedOrders = [];
+      for (const key in resData) {
+        loadedOrders.push(
+          new Orders(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(reData[key].date)
+          )
+        );
+      }
+      dispatch({
+        type: SET_PRODUCTS,
+        orders: loadedOrders,
+      });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
 export const addOrder = (cartItems, totalAmount) => {
   return async (dispatch) => {
     const date = new Date();
