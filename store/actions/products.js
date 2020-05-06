9956e53b50f9e9aa99a7e3bf0f770facf9b01162
hyetigran <tigran.asriyan@gmail.com>
2020-05-05 21:44:28 -0400

    add async action and reducer for orders

--- store/actions/products.js
+++ store/actions/products.js
@@ -40,12 +40,15 @@ export const fetchProducts = () => {
 
 export const deleteProduct = (productId) => {
   return async (dispatch) => {
-    await fetch(
+    const response = await fetch(
       `https://rn-guide-7456a.firebaseio.com/products/${productId}.json`,
       {
         method: "DELETE",
       }
     );
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
     dispatch({ type: DELETE_PRODUCT, pid: productId });
   };
 };
@@ -81,13 +84,19 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async (dispatch) => {
-    await fetch(`https://rn-guide-7456a.firebaseio.com/products/${id}.json`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ title, description, imageUrl }),
-    });
+    const response = await fetch(
+      `https://rn-guide-7456a.firebaseio.com/products/${id}.json`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, description, imageUrl }),
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
     dispatch({
       type: UPDATE_PRODUCT,
       pid: id,
